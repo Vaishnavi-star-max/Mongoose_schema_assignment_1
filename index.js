9690c7bc392488dc700c8a4231dccb3ba60caecc
pallempativaishnavi@gmail.com <pallempativaishnavi@gmail.com>
2025-02-19 15:13:06 +0530

    Schema-1

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose =require('mongoose')
+const {User}=require('./schema')
+const UserModel=require('./schema').User;
+const ProfileModel=require('./schema').Profile;
+const TrackingModel=require('./schema').Tracking;
 
 const app = express();
-const port = 3010;
+const port = 7686;
+
+const mongoDB='mongodb+srv://pallempativaishnavi:3pasP6QvdcOrw8Gy@cluster0.q2w5m.mongodb.net/schema1?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.connect(mongoDB);
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+
+app.post('/test-create-user', async (req, res) => {
+  try {
+    const newUser = new UserModel({
+      username: 'testuser',
+      email: 'testuser@example.com',
+      password: 'password123',
+      roles: ['user']
+    });
+    await newUser.save();
+    res.status(201).send('User created successfully');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ schema.js
@@ -0,0 +1,47 @@
+const mongoose= require("mongoose")
+
+const userSchema=new mongoose.Schema({
+    username: {type: String,
+               required:true,
+               unique:true},
+
+    email: {type: String,
+             require:true,
+             unique:true },
+
+    password: {type:String,
+             require:true },
+
+    roles: {type:String,
+            require:true},      
+})
+
+const ProfileSchema=new mongoose.Schema({
+    user:{type: mongoose.Schema.Types.ObjectId,
+          ref:'User' },
+
+    firstname: {type:String,
+                required:true},
+
+    lastname: {type:String,
+                require:true},  
+              
+        age: { type:Number,
+                require:true},       
+                           
+})
+
+const TrackingSchema=new mongoose.Schema({
+    user:{type: mongoose.Schema.Types.ObjectId,
+        ref:'User' },
+    lastlogin:{type:Date,
+               require:true},
+
+     
+})
+
+const UserModel=mongoose.model('User',userSchema);
+const ProfileModel=mongoose.model('Profile',ProfileSchema);
+const TrackingModel=mongoose.model('Tracking',TrackingSchema);
+
+module.exports=UserModel;
